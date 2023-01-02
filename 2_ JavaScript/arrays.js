2c464d01e0c84a02f565acdb85f2f421dcb0abf0
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-01-02 20:11:36 +0530

    js fundamentals

---
+++ 2_ JavaScript/arrays.js
@@ -0,0 +1,8 @@
+
+// Array is just an indexed collection of elements in javascript
+
+// Array declaration
+
+let arr = []
+arr = [1, 2, 3, 4, 'hello', 'hi', 'bye', true, 2.5]
+console.log(arr);
---
+++ 2_ JavaScript/variable.js
@@ -0,0 +1,68 @@
+// JavaScript is a dynamically typed language.where as java is a statically typed language.
+
+// var a = 12.34;
+//console.log(a); // 12.34
+
+// var a = 'my name is steve'
+// console.log(a); 
+// // var keyword problem is that it can be Redeclared.
+
+// a = 'who are you'
+// console.log(a) // var an be reassigned.
+
+// // TO solve the var keyword problem we use let keyword--
+
+//let b = 'pepcoding'
+//console.log(b);
+
+// let b = 12
+// console.log(b) // here redeclaration problem has been solved.
+
+//c = 20
+//console.log(c); // let can also be reassigned.
+
+// var is function scoped and let is block scoped
+
+// function hello(){
+//     var a = 10
+//     console.log(a); // 10
+
+// }
+// hello()
+// console.log(a); // this is not allowed
+
+// if(true){
+//     var b =2
+//     console.log(b);// 2
+// }
+// console.log(b);// 2  downside of using var 
+
+// if(true){
+//     let a = 20
+//     console.log(a);
+// }// this is block
+// console.log(a); // this is not allowed.
+
+// code snippet
+
+// var b  = true;
+// if(b == true){
+//     var a = 23;
+//     console.log(a);// 23
+// }
+// console.log(a); // 23 var is function scope not block scope
+
+const a = 2; // declaration
+console.log(a);
+
+// const a = 20; // No redeclaration
+// console.log(a);
+
+a = 'hello'; // No reassigning
+console.log(a);
+
+
+
+
+
+
