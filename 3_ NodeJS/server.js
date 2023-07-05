ec8119e6c4e814204d2c5123d9d4b052ee806f33
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-07-05 23:06:39 +0530

    about request and response

--- 3_ NodeJS/server.js
+++ 3_ NodeJS/server.js
@@ -1,7 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
          console.log(req.url, req.method);
+
+        //  res.setHeader("content-type", "text/plain");
+        res.setHeader("content-type", "text/html");
+
+        // res.write("<head><link rel='stylesheet' href='#'></head>")
+        //  res.write("<h1>hello there</h1>");
+        //  res.write("<h2>hello there</h2>");
+        //  res.end();
+
+    fs.readFile('./view/index.html')
+
 });
 server.listen(3000, "localhost", () => {
   console.log("listening for request on port number 3000");
