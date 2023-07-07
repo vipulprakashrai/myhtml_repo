1e2e4bc7f042380b11de142c53538b1ee8418ead
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-07-07 23:49:46 +0530

    redirects

--- 3_ NodeJS/server.js
+++ 3_ NodeJS/server.js
@@ -17,6 +17,10 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
+            case '/about-me':
+              res.statusCode = 301;
+              res.setHeader('Location', '/about');
+              break;  
           default:
             path += '404.html';
             res.statusCode = 400;
