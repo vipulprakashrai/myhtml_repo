586a936f1a46d4d312d372f05408132ccd14d1c4
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-07-12 23:58:14 +0530

    ejs view engine

--- 3_ NodeJS/app.js
+++ 3_ NodeJS/app.js
@@ -1,27 +1,27 @@
 const express = require("express");
 
 const app = express();
+
+// register view engine->
+app.set("view engine", "ejs");
+app.set("views", "myviews")
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res)=> {
-    //res.send("<p>Namaste Guys!</p>");
-    res.sendFile('./view/index.html', {root: __dirname})
+   res.render("index");
 });
 
 app.get('/about', (req, res)=> {
-    //res.send("<p>About Page!</p>");
-    res.sendFile('./view/about.html', {root: __dirname});
+    res.render("about");
 });
 
-// redirects
-
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) =>{
+     res.render("create");
 });
-
 // 404 page
 
 app.use((req, res)=>{
-    res.setStatus().sendFile('./view/404.html', {root: __dirname});
+    res.status(404).render("404");
 });
