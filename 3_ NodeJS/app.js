3cdefe863ad75e9a20b19a2103b85b1faa6b4b89
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-07-18 23:59:53 +0530

    middleware

--- 3_ NodeJS/app.js
+++ 3_ NodeJS/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+
 
 // register view engine->
 app.set("view engine", "ejs");
@@ -9,19 +10,9 @@ app.set("views", "myviews")
 // listen for request
 app.listen(3000);
 
-// creating a custom middleware 
-app.use((req, res, next)=>{
-    console.log("new request made");
-    console.log('host: ' , req.hostname);
-    console.log('path: ', req.path);
-    console.log('method:', req.method);
-    next();
-});
-
-app.use((req, res, next)=>{
-    console.log("In the next middleware");
-    next();
-});
+// middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res)=> {
     const blogs = [
